e268a57da94f7029eab14452bac7e2573bfab3d6
muhammetguducu <muhammetguducu@outlook.de>
2026-07-19 21:30:47 +0200

    chore: initial commit

---
+++ src/main/java/com/mgudux/ifas/IfasApplication.java
@@ -0,0 +1,13 @@
+package com.mgudux.ifas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgudux/ifas/IfasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgudux.ifas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
