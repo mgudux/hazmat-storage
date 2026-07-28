60f50f57d9869450bf9a43981300a30afdc52bcc
muhammetguducu <muhammetguducu@outlook.de>
2026-07-28 23:12:55 +0200

    KAN-9: chore: add enums and minor changes to flowchart to represent new business rule

--- src/main/java/com/mgudux/ifas/domain/entity/enums/IndustryType.java
+++ src/main/java/com/mgudux/ifas/domain/entity/enums/IndustryType.java
@@ -1,4 +1,20 @@
 package com.mgudux.ifas.domain.entity.enums;
 
+
+import java.util.EnumSet;
+import java.util.Set;
+
 public enum IndustryType {
+    LOGISTIC(EnumSet.of(StorageType.SHELVES, StorageType.PRESSURE)),
+    CHEMICAL(EnumSet.of(StorageType.COMPOST, StorageType.CRYOGENIC));
+
+    private final Set<StorageType> allowedStorageTypes;
+
+    IndustryType(Set<StorageType> allowedStorageTypes) {
+        this.allowedStorageTypes = allowedStorageTypes;
+    }
+
+    public boolean isStorageTypeAllowed(StorageType storageType) {
+        return this.allowedStorageTypes.contains(storageType);
+    }
 }
--- src/main/java/com/mgudux/ifas/domain/entity/enums/StorageType.java
+++ src/main/java/com/mgudux/ifas/domain/entity/enums/StorageType.java
@@ -1,4 +1,8 @@
 package com.mgudux.ifas.domain.entity.enums;
 
 public enum StorageType {
+    SHELVES,
+    PRESSURE,
+    CRYOGENIC,
+    COMPOST
 }
