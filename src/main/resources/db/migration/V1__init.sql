f35a3038274df6b5cea88840ddb2a24f4a243698
muhammetguducu <muhammetguducu@outlook.de>
2026-07-28 20:07:53 +0200

    KAN-9: fix: replace DATETIME with TIMESTAMP for H2 Database compability

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -3,8 +3,8 @@ CREATE TABLE company (
     name VARCHAR(255) NOT NULL,
     address VARCHAR(255) NOT NULL,
     industry VARCHAR(255) NOT NULL,
-    updated DATETIME NOT NULL,
-    created DATETIME NOT NULL);
+    updated TIMESTAMP NOT NULL,
+    created TIMESTAMP NOT NULL);
 
 CREATE TABLE storage (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -16,8 +16,8 @@ CREATE TABLE storage (
     capacityUnit VARCHAR(255) NOT NULL,
     lastCheck TIMESTAMP NOT NULL,
     monthsCheckIntervall INT NOT NULL,
-    updated DATETIME NOT NULL,
-    created DATETIME NOT NULL,
+    updated TIMESTAMP NOT NULL,
+    created TIMESTAMP NOT NULL,
 
     CONSTRAINT fk_storage_company FOREIGN KEY (companyId) REFERENCES company(id)
 );
@@ -29,9 +29,9 @@ CREATE TABLE content (
     hazard VARCHAR(255),
     amount DOUBLE NOT NULL,
     amountUnit VARCHAR(255) NOT NULL,
-    expirationDate DATETIME,
-    updated DATETIME NOT NULL,
-    created DATETIME NOT NULL,
+    expirationDate TIMESTAMP,
+    updated TIMESTAMP NOT NULL,
+    created TIMESTAMP NOT NULL,
 
     CONSTRAINT fk_content_storage FOREIGN KEY (storageId) REFERENCES storage(id)
 
